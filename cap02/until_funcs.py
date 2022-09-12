512b7bf75042286ef2d246262ab4a856935636e1
Gabriel Alves Oliveira <111614350+oliveiraprg@users.noreply.github.com>
2022-09-12 17:51:08 -0300

    use of some native python functions

---
+++ cap02/until_funcs.py
@@ -0,0 +1,14 @@
+# demostrate built-in untility functions
+
+
+def main():
+    # Use any() e all() para testar valores booleanos
+    lista = [1, 2, 3, 5, 4, 6]
+    print(any(lista))
+    print(all(lista))
+    print(min(lista))
+    print(max(lista))
+    print(sum(lista))
+
+if __name__ == "__main__":
+    main()
